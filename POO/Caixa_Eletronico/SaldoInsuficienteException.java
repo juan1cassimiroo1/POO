28010eed4322ac7057101d889b5a2891729a109b
Juan Cassimiro <80433936+juan1cassimiroo1@users.noreply.github.com>
2026-05-29 08:31:19 -0300

    Delete POO/Caixa_Eletronico directory

--- POO/Caixa_Eletronico/SaldoInsuficienteException.java
+++
@@ -1,6 +0,0 @@
-public class SaldoInsuficienteException extends Exception {
-    public SaldoInsuficienteException(String mensagem) {
-        // Repassa a mensagem para a classe super
-        super(mensagem); 
-    }
-}
--- POO/Caixa_Eletronico/ValorInvalidoException.java
+++
@@ -1,5 +0,0 @@
-public class ValorInvalidoException extends Exception {
-    public ValorInvalidoException(String mensagem) {
-        super(mensagem);
-    }
-}
